42ed32a71ad971bc2432181788042ce563c83b35
rd316 <rd316.gortor20@gmail.com>
2025-07-29 07:22:02 +0300

    add the build logic

---
+++ rstr_client/build.rs
@@ -0,0 +1,23 @@
+use std::env;
+
+#[cfg(windows)]
+fn build_win_rc() {
+    extern crate windres;
+    use windres::Build;
+
+    println!("cargo:rerun-if-changed=rstr.rc");
+    if cfg!(windows) { 
+        Build::new().compile("rstr.rc").unwrap();
+    }
+}
+
+#[cfg(unix)]
+fn build_win_rc() {}
+
+fn main() {
+    println!("cargo:rerun-if-changed=ui/");
+    unsafe { env::set_var("SLINT_BACKEND", "winit-skia") };
+    slint_build::compile("ui/app-window.slint").unwrap();
+
+    build_win_rc();
+}
